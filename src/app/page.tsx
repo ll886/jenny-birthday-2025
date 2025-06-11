4e9fe5ff2dc11fe7c2155d1ca33762fb00afa324
Larry <13326841-larryfitquest@users.noreply.gitlab.com>
2025-06-11 00:35:41 -0400

    remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, Suspense, useEffect, useRef } from "react";
+import { useState, Suspense } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 
